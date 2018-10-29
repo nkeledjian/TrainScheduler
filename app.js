4b330cca519d5ce3be701d95f8975550798835c5
nVar <nkeledjian@gmail.com>
2018-10-29 11:31:45 -0700

    Styling update to html page; working on displaying user entries to page and minutes away time

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var config = {
     messagingSenderId: "10517861927"
   };
   firebase.initializeApp(config);
-
+// store database in dataRef
 var dataRef = firebase.database();  
 
 var TrainName = "";
@@ -16,24 +16,44 @@ var Dest = "";
 var Freq = "";
 var NextArriv = "";
 var MinAway = "";
-
-  // point to HTML handling input
-$("#add-train").on("click", function(event) {
+var l = console.log;
+// point to HTML handling input
+$("#add-train-btn").on("click", function(event) {
 
     event.preventDefault();
-
-    TrainName = $("#inputTrainName").val();
-    Dest = $("#inputDest").val();
-    Freq = $("#inputFreq").val();
-    NextArriv = $('#inputNextArriv').val();
-    MinAway = $("#inputMinAway").val();
-
-    dataRef.ref().push({
-            TrainName: TrainName,
+    // grabs user input
+    TrainName = $("#inputTrainName").val().trim();
+    Dest = $("#inputDest").val().trim();
+    Freq = $("#inputFreq").val().trim();
+    NextArriv = $('#inputNextArriv').val().trim();
+    // MinAway = $("#inputMinAway").val().trim();
+    // Store the temporary objects in a variable
+    newTrain = {
+            Train: TrainName,
             Dest: Dest,
             Freq: Freq,
-            NextArriv: NextArriv,
-            MinAway: MinAway,
-        });
+            Next: NextArriv,
+            // MinAway: MinAway,
+        };
 
+    alert("New train details successfully added");
+    // push newTrain var to firebase database
+    dataRef.ref().push(newTrain);
+    // Console logging local object data
+    l(newTrain.Train);
+    l(newTrain.Dest);
+    l(newTrain.Freq);
+    l(newTrain.Next);
+    // l(newTrain.MinAway);
+
+    // Clears input field for next new Train entry
+    $("#inputTrainName").val("");
+    $("#inputDest").val("");
+    $("#inputFreq").val("");
+    $("#inputNextArriv").val("");
+});
+
+// function to handle appending user's entry's to page
+database.ref().on("child_added", function(childSnapshot) { 
+    
 });
