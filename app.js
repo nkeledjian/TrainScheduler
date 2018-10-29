194b745a156cc94db0fe43a647220d9c0d171c10
nVar <nkeledjian@gmail.com>
2018-10-29 09:42:59 -0700

    Initial setup of files - setting up firebase and html page

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB49Eg43MVQhsMzgumwbAZFmmsZi1IrdcE",
+    authDomain: "trainscheduler-ee02e.firebaseapp.com",
+    databaseURL: "https://trainscheduler-ee02e.firebaseio.com",
+    projectId: "trainscheduler-ee02e",
+    storageBucket: "trainscheduler-ee02e.appspot.com",
+    messagingSenderId: "10517861927"
+  };
+  firebase.initializeApp(config);
